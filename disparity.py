95d5f485e3d27f741160b2db969f2976ed667f03
Goh Xing Yang <69152768+Neva-Y@users.noreply.github.com>
2022-10-20 14:00:13 +1100

    Multiprocessing implemented

--- disparity.py
+++ disparity.py
@@ -3,14 +3,12 @@ import math
 import matplotlib.pyplot as plt 
 import numpy as np
 import cv2
-from tqdm import tqdm
-import sys
-import concurrent.futures
+import multiprocessing as mp
+import time
 
 # Import images 
 directory = './Dataset/'
 allfiles = os.listdir(directory)
-
 filenames = []
 
 for filename in allfiles:
@@ -21,7 +19,7 @@ for filename in allfiles:
 
 # Perform on first image for now 
 
-file = filenames[0]
+file = filenames[3]
 
 left_image_name = directory + file + 'left.jpg'
 right_image_name = directory+ file + 'right.jpg'
@@ -40,77 +38,146 @@ img_r_grey = img_r_grey_full
 img_l = img_l[...,::-1]
 img_r = img_r[...,::-1]
 
-img_d = cv2.imread(disp_image_name)
-img_d_grey = np.int32(cv2.cvtColor(img_d, cv2.COLOR_BGR2GRAY))
-
 
 # note: can perform downscaling of the images to speed up computation at the cost of quality degradation
-# scale_percentage = 50
-# width = int(img_l_grey_full.shape[1] * scale_percentage / 100)
-# height = int(img_l_grey_full.shape[0] * scale_percentage / 100)
-# dim = (width, height)
+scale_percentage = 100
+width = int(img_l_grey_full.shape[1] * scale_percentage / 100)
+height = int(img_l_grey_full.shape[0] * scale_percentage / 100)
+dim = (width, height)
 
-# img_l_grey = np.int32(cv2.resize(img_l_grey_full.astype('float32'), dim, interpolation = cv2.INTER_AREA))
-# img_r_grey = np.int32(cv2.resize(img_r_grey_full.astype('float32'), dim, interpolation = cv2.INTER_AREA))
+img_l_grey = np.int32(cv2.resize(img_l_grey_full.astype('float32'), dim, interpolation = cv2.INTER_AREA))
+img_r_grey = np.int32(cv2.resize(img_r_grey_full.astype('float32'), dim, interpolation = cv2.INTER_AREA))
 
-# Optised plotting of the disparity map
-h,w = img_l_grey.shape
-calc_disp2 = np.zeros((h,w))
-boundary_size = 10
+h,w = img_r_grey.shape
+
+boundary_size = 3
 neighbourhood_l = 200
-nighbourhood_r = 50
+neighbourhood_r = 50
 step_size = 1
 measure = "NCC"
 
-def compute_disparity_for_row(row_index):
-    row = img_l_grey[row_index]
+# Take horizontal window to search, ensuring values don't exceed bounds
+min_bound = np.int32(boundary_size)
+max_bound = np.int32(len(img_l_grey[0])-boundary_size)
 
-    for col_index_left in range(boundary_size, len(row)-boundary_size, step_size):
-        
+img_d = cv2.imread(disp_image_name,-1)/256.0
+
+from skimage.graph import shortest_path, route_through_array, MCP
+# Code adapted from skimage.shortest_path algorithm
+def my_shortest_path(arr, reach=1, axis=-1, output_indexlist=False):
+    # First: calculate the valid moves from any given position. Basically,
+    # always move +1 along the given axis, and then can move anywhere within
+    # a grid defined by the reach.
+    if axis < 0:
+        axis += arr.ndim
+    offset_ind_shape = (2 * reach + 1,) * (arr.ndim - 1)
+    offset_indices = np.indices(offset_ind_shape) - reach
+    offset_indices = np.insert(offset_indices, axis,
+                               np.ones(offset_ind_shape), axis=0)
+    offset_size = np.multiply.reduce(offset_ind_shape)
+    offsets = np.reshape(offset_indices, (arr.ndim, offset_size), order='F').T
+
+    # Valid starting positions are anywhere on the hyperplane defined by
+    # position 0 on the given axis. Ending positions are anywhere on the
+    # hyperplane at position -1 along the same.
+    non_axis_shape = arr.shape[:axis] + arr.shape[axis + 1:]
+    non_axis_indices = np.indices(non_axis_shape)
+    non_axis_size = np.multiply.reduce(non_axis_shape)
+    start_indices = np.insert(non_axis_indices, axis,
+                              np.zeros(non_axis_shape), axis=0)
+    starts = np.reshape(start_indices, (arr.ndim, non_axis_size), order='F').T[0:1]
+    end_indices = np.insert(non_axis_indices, axis,
+                            np.full(non_axis_shape, -1,
+                                    dtype=non_axis_indices.dtype), axis=0)
+    ends = np.reshape(end_indices, (arr.ndim, non_axis_size), order='F').T[-2:-1]
+    
+    # Find the minimum-cost path to one of the end-points
+    m = MCP(arr, offsets=offsets)
+    costs, traceback = m.find_costs(starts, ends, find_all_ends=True)
+
+    # Figure out which end-point was found
+    for end in ends:
+        cost = costs[tuple(end)]
+        if cost != np.inf:
+            break
+    traceback = m.traceback(end)
+
+    if not output_indexlist:
+        traceback = np.array(traceback)
+        traceback = np.concatenate([traceback[:, :axis],
+                                    traceback[:, axis + 1:]], axis=1)
+        traceback = np.squeeze(traceback)
+
+    return traceback, cost
+
+def compute_row(row_index):
+    row = img_l_grey[row_index]
+    dsi = []
+    for col_index_left in range(boundary_size,len(row)-boundary_size, step_size):
+
         centre_l = [row_index, col_index_left]
 
         min_l_x = centre_l[1] - boundary_size
-        max_l_x = centre_l[1] + boundary_size
+        max_l_x = centre_l[1] + boundary_size+1
 
         min_l_y = centre_l[0] - boundary_size
-        max_l_y = centre_l[0] + boundary_size
+        max_l_y = centre_l[0] + boundary_size+1
 
         patch_l = img_l_grey[min_l_y:max_l_y, min_l_x:max_l_x]
 
-        # Take horizontal window to search, ensuring values don't exceed bounds
-        min_bound = max(col_index_left-neighbourhood_l, boundary_size)
-        max_bound = min(col_index_left+nighbourhood_r, len(row)-boundary_size)
-        
         # Indices of all column centres to check
         col_centres = np.arange(min_bound,max_bound) 
-        
+
         # Generate arrays for each column centre in steps of 1 using the boundary size, each having 2*boundary_size elements
-        col_linspace = np.int32(np.transpose(np.linspace(col_centres-boundary_size, col_centres+boundary_size, 2*boundary_size)))
+        col_linspace = np.int32(np.transpose(np.linspace(col_centres-boundary_size, col_centres+boundary_size, 2*boundary_size+1)))
 
         # Generate right patch using the constant rows in loop iteration and column arrays, transpose to get correct shape
+
         patch_r = np.transpose(img_r_grey[min_l_y:max_l_y, col_linspace], (1,0,2)) 
-        
+
+
         # Choose distance measure to generate disparity map
         if measure == "NCC":
             norm_corr = np.sum(patch_l*patch_r, axis=(1,2))/(np.linalg.norm(patch_l)*np.linalg.norm(patch_r, axis=(1,2)))
+            dsi.append(norm_corr)
             min_col_index = np.argmax(norm_corr) + min_bound - 1
-            calc_disp2[row_index][col_index_left] = abs(min_col_index - col_index_left)
-
+            
         elif measure == "SSD":
             error = np.sum((patch_l-patch_r)**2, axis=(1,2))
-            min_col_index = np.argmin(error) + min_bound - 1
-            calc_disp2[row_index][col_index_left] = abs(min_col_index - col_index_left)
+            dsi.append(error)
+            min_col_indexx = np.argmin(error) + min_bound - 1
+    
+    arr = np.transpose(np.array(dsi))
+    if measure == "NCC":
+        arr = np.array([[ 0.001 if i > j or i+150 < j else arr[i,j] for j in range(len(arr))] for i in range(len(arr))])
+        arr = 1/(arr)
+        
+    elif measure == "SSD":
+        arr = np.array([[ np.inf if i > j else arr[i,j] for j in range(len(arr))] for i in range(len(arr))])
+    
+    p, cost = my_shortest_path(arr, reach=5)
+    p1 = np.arange(0,len(arr))
+    points = np.array([[p[i], p1[i]] for i in range(len(p))])
+    disp = abs(np.subtract(points[:,1], points[:,0]))
 
-
+    return disp
+    
 def main():
-    with concurrent.futures.ProcessPoolExecutor() as executor:
-        row_index = [i for i in range(boundary_size, len(img_l_grey)-boundary_size, step_size)]
-        executor.map(compute_disparity_for_row, row_index)
-    plt.imshow(calc_disp2, cmap='gray', vmin=0, vmax=255)
-    plt.savefig("disparity_parallel.png")
-    plt.show()
+    start = time.time()
+    calc_disp2 = np.zeros((h,w))
+    row_index = [i for i in range(boundary_size, len(img_l_grey)-boundary_size, step_size)]
+    with mp.Pool(processes = mp.cpu_count()-1) as pool:
+        disp_result = pool.map(compute_row, row_index)
+        pool.close()
+        pool.join()
+        disp_result = np.array(disp_result)
+        calc_disp2[row_index,min_bound:max_bound] = disp_result
+        end = time.time()
+        print(end-start)
+        plt.imshow(calc_disp2, cmap='plasma',vmin=0)
+        plt.show()
+        plt.savefig("disparity_parallel.png")
+        plt.close()
 
 if __name__ == '__main__':
     main()
-
-
