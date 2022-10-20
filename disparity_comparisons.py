479d5bcaa3525770da8f16bef45f1c49bf5f65ee
Goh Xing Yang <69152768+Neva-Y@users.noreply.github.com>
2022-10-20 16:52:19 +1100

    Generate comparisons with multiprocessing

--- disparity_comparisons.py
+++ disparity_comparisons.py
@@ -42,17 +42,15 @@ img_d = cv2.imread(disp_image_name,-1)/256.0
 
 
 # note: can perform downscaling of the images to speed up computation at the cost of quality degradation
-scale_percentage = 100
-width = int(img_l_grey_full.shape[1] * scale_percentage / 100)
-height = int(img_l_grey_full.shape[0] * scale_percentage / 100)
-dim = (width, height)
-
-img_l_grey = np.int32(cv2.resize(img_l_grey_full.astype('float32'), dim, interpolation = cv2.INTER_AREA))
-img_r_grey = np.int32(cv2.resize(img_r_grey_full.astype('float32'), dim, interpolation = cv2.INTER_AREA))
-img_d = np.int32(cv2.resize(img_d.astype('float32'), dim, interpolation = cv2.INTER_AREA))
+# scale_percentage = 30
+# width = int(img_l_grey_full.shape[1] * scale_percentage / 100)
+# height = int(img_l_grey_full.shape[0] * scale_percentage / 100)
+# dim = (width, height)
+# img_l_grey = np.int32(cv2.resize(img_l_grey_full.astype('float32'), dim, interpolation = cv2.INTER_AREA))
+# img_r_grey = np.int32(cv2.resize(img_r_grey_full.astype('float32'), dim, interpolation = cv2.INTER_AREA))
+# img_d = np.int32(cv2.resize(img_d.astype('float32'), dim, interpolation = cv2.INTER_AREA))
 
 h,w = img_r_grey.shape
-
 boundary_size = 3
 neighbourhood_l = 200
 neighbourhood_r = 50
@@ -112,7 +110,48 @@ def my_shortest_path(arr, reach=1, axis=-1, output_indexlist=False):
 
     return traceback, cost
 
-def compute_row(row_index):
+def compute_row_nosmooth(row_index):
+    row = img_l_grey[row_index]
+    disp = []
+    for col_index_left in range(boundary_size,len(row)-boundary_size, step_size):
+        
+        centre_l = [row_index, col_index_left]
+
+        min_l_x = centre_l[1] - boundary_size
+        max_l_x = centre_l[1] + boundary_size + 1
+
+        min_l_y = centre_l[0] - boundary_size
+        max_l_y = centre_l[0] + boundary_size + 1
+
+        patch_l = img_l_grey[min_l_y:max_l_y, min_l_x:max_l_x]
+
+        # Take horizontal window to search, ensuring values don't exceed bounds
+        min_bound = max(col_index_left-neighbourhood_l, boundary_size)
+        max_bound = min(col_index_left+neighbourhood_r, len(row)-boundary_size)
+        
+        # Indices of all column centres to check
+        col_centres = np.arange(min_bound,max_bound) 
+        
+        # Generate arrays for each column centre in steps of 1 using the boundary size, each having 2*boundary_size elements
+        col_linspace = np.int32(np.transpose(np.linspace(col_centres-boundary_size, col_centres+boundary_size, 2*boundary_size+1)))
+
+        # Generate right patch using the constant rows in loop iteration and column arrays, transpose to get correct shape
+        patch_r = np.transpose(img_r_grey[min_l_y:max_l_y, col_linspace], (1,0,2)) 
+        
+        # Choose distance measure to generate disparity map
+        if measure == "NCC":
+            norm_corr = np.sum(patch_l*patch_r, axis=(1,2))/(np.linalg.norm(patch_l)*np.linalg.norm(patch_r, axis=(1,2)))
+            min_col_index = np.argmax(norm_corr) + min_bound
+            disp.append(abs(min_col_index - col_index_left))
+
+        elif measure == "SSD":
+            error = np.sum((patch_l-patch_r)**2, axis=(1,2))
+            min_col_index = np.argmin(error) + min_bound
+            disp.append(abs(min_col_index - col_index_left))
+        
+    return disp
+
+def compute_row_path(row_index):
     row = img_l_grey[row_index]
     dsi = []
     for col_index_left in range(boundary_size,len(row)-boundary_size, step_size):
@@ -120,10 +159,10 @@ def compute_row(row_index):
         centre_l = [row_index, col_index_left]
 
         min_l_x = centre_l[1] - boundary_size
-        max_l_x = centre_l[1] + boundary_size+1
+        max_l_x = centre_l[1] + boundary_size + 1
 
         min_l_y = centre_l[0] - boundary_size
-        max_l_y = centre_l[0] + boundary_size+1
+        max_l_y = centre_l[0] + boundary_size + 1
 
         patch_l = img_l_grey[min_l_y:max_l_y, min_l_x:max_l_x]
 
@@ -142,12 +181,10 @@ def compute_row(row_index):
         if measure == "NCC":
             norm_corr = np.sum(patch_l*patch_r, axis=(1,2))/(np.linalg.norm(patch_l)*np.linalg.norm(patch_r, axis=(1,2)))
             dsi.append(norm_corr)
-            min_col_index = np.argmax(norm_corr) + min_bound - 1
             
         elif measure == "SSD":
             error = np.sum((patch_l-patch_r)**2, axis=(1,2))
             dsi.append(error)
-            min_col_indexx = np.argmin(error) + min_bound - 1
     
     arr = np.transpose(np.array(dsi))
     if measure == "NCC":
@@ -180,33 +217,57 @@ def compute_RMSE(calc_disp2):
 
 def main():
     start = time.time()
+    calc_disp1 = np.zeros((h,w))
     calc_disp2 = np.zeros((h,w))
+    bilateral_disp = np.zeros((h,w))
     row_index = [i for i in range(boundary_size, len(img_l_grey)-boundary_size, step_size)]
+
     with mp.Pool(processes = mp.cpu_count()-1) as pool:
-        disp_result = pool.map(compute_row, row_index)
+        disp_result = pool.map(compute_row_nosmooth, row_index)
+        pool.close()
+        pool.join()
+        disp_result = np.array(disp_result)
+        calc_disp1[row_index,min_bound:max_bound] = disp_result
+        bilateral_disp = cv2.bilateralFilter(np.uint8(calc_disp1),7,50,50)
+
+    with mp.Pool(processes = mp.cpu_count()-1) as pool:
+        disp_result = pool.map(compute_row_path, row_index)
         pool.close()
         pool.join()
         disp_result = np.array(disp_result)
         calc_disp2[row_index,min_bound:max_bound] = disp_result
 
-        # dim = (img_l_grey_full.shape[1], img_l_grey_full.shape[0])
-        # calc_disp2 = np.int32(cv2.resize(calc_disp2.astype('float32'), dim, interpolation = cv2.INTER_AREA))
 
-        print("RMSE with shortest path smoothing")
-        compute_RMSE(calc_disp2)
-        end = time.time()
-        print(f"Time to process disparity image: {end-start:.2f}s")
+    print()
+    print("RMSE with no smoothing:")
+    compute_RMSE(calc_disp1)
+    print()
+    print("RMSE with bilateral filter smoothing:")
+    compute_RMSE(bilateral_disp)
+    print()    
+    print("RMSE with shortest path smoothing:")
+    compute_RMSE(calc_disp2)
+    end = time.time()
+    print(f"Time to process disparity images: {end-start:.2f}s")
 
-        plt.subplots(1,2, figsize=(15,10))
-        plt.subplot(1,2,1)
-        plt.imshow(calc_disp2, cmap='plasma',vmin=0)
-        plt.title('Disparity image using shortest path')
+    plt.subplots(2,2, figsize=(15,10))
+    plt.subplot(2,2,1)
+    plt.imshow(calc_disp1, cmap='plasma',vmin=0)
+    plt.title('Disparity image with no smoothing')
 
-        plt.subplot(1,2,2)
-        plt.imshow(img_d, cmap='plasma',vmin=0)
-        plt.title('Ground truth')
-        plt.show()
-        plt.close()
+    plt.subplot(2,2,2)
+    plt.imshow(bilateral_disp, cmap='plasma',vmin=0)
+    plt.title('Disparity image with bilateral filter smoothing')
+
+    plt.subplot(2,2,3)
+    plt.imshow(calc_disp2, cmap='plasma',vmin=0)
+    plt.title('Disparity image using shortest path')
+
+    plt.subplot(2,2,4)
+    plt.imshow(img_d, cmap='plasma',vmin=0)
+    plt.title('Ground truth')
+    plt.show()
+    plt.close()
 
 if __name__ == '__main__':
     main()
